89491d5891223a628f5e33e405af85fb59d8bb5a
Anandwutti <wuttianand@gmail.com>
2019-07-30 21:41:31 +0700

    Updates

---
+++ precache-manifest.36cc62cf6b0492f9fc523f8531a0bf89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd9fa244f8257fabfffa8afc8104f6c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dc9f9084c8098c8af0d1",
+    "url": "/monster-rolodex/static/css/main.5a132398.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dc9f9084c8098c8af0d1",
+    "url": "/monster-rolodex/static/js/main.3604ee3d.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
